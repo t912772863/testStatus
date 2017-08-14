571b10a3e25231c106385e60064b9d60661c5a67
t912772863 <912772863@qq.com>
2017-08-14 18:25:54 +0800

    ..............

--- src/com/test/teststatus/servlet/InitServlet.java
+++ src/com/test/teststatus/servlet/InitServlet.java
@@ -7,9 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-
-import static com.test.teststatus.db.OperOracle.getCompanyInfo;
+import java.io.OutputStream;
 
 /**
  * Created by Administrator on 2017/8/7 0007.
@@ -18,6 +16,7 @@ public class InitServlet extends HttpServlet {
     private static Logger logger = Logger.getLogger(InitServlet.class);
     @Override
     public void init() throws ServletException {
+        // TODO: 2017/8/14 0014 这里可以配置要初始化加载的资源
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e1) {
@@ -31,21 +30,22 @@ public class InitServlet extends HttpServlet {
         doPost(req,resp);
     }
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
         try{
-            logger.info("Thread: "+Thread.currentThread().getName());
-            getCompanyInfo();
+            request.setCharacterEncoding("UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            // 根据不同的请求命令, 调用不同的方法
+            String commandName = request.getParameter("commandName");
+            OutputStream out = response.getOutputStream();
 
-            Map map = req.getParameterMap();
-            for(Object s : map.keySet()){
-                logger.info("param: "+s+" = "+((String[])map.get(s))[0]);
+            if("login".equals(commandName)){
+                // 登录方法
+                out.write("登录成功".getBytes());
+
+            }else {
+                // 找不到匹配的方法,返回未知命令
+                out.write("未知的方法命令".getBytes());
             }
-            resp.getWriter().write("<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-                    "<root>"+
-                    "<RetCode>0</RetCode>"+
-                    "<RetMsg>success</RetMsg>"+
-                    "</root>");
-
         }catch (Exception e){
             logger.info(e.getLocalizedMessage());
         }
